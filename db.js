c6a01794d817ee0cb3931a92565ae8d0de72bed2
jeanmw <jean.weatherwax@gmail.com>
2017-05-01 11:20:09 -0700

    mongo

--- db.js
+++ db.js
@@ -6,9 +6,9 @@ var config    = require('./config'),
     mongoose  = require('mongoose');
 
 module.exports = function() {
-  var db = mongoose.connect(config.db);
+  var db = mongoose.connect(process.env.mongolab-rugged-78843 || 'mongodb://localhost/questionqueue');
   require('./models/post');
   require('./models/user');
-  
+
   return db;
 };
